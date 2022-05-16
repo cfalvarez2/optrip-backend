a2967109d0308c7a184b1393ebad4c5f4ae2d5e9
Catalina Alvarez <catalina.alvarez@pinflag.cl>
2022-05-16 19:59:54 -0400

    Revert "init proyect with template"
    This reverts commit 0e3251f283f34db7ce445d11a5a562f80392ef99.

--- src/controllers/complex_tests/__init__.py
+++
@@ -1,1 +0,0 @@
-from .controller import test_controller  # noqa: F401
--- src/router.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from .routes import index, v1
-
-
-router = APIRouter()
-
-# Import routers
-router.include_router(index, include_in_schema=False)
-router.include_router(v1.router, prefix="/v1")
