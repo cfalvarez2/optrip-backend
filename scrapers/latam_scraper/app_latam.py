ecf1110c4d7e4b9bd321dc7323fac63f80a0bcc6
tomasjelvez <tjelvez@uc.cl>
2022-06-24 11:24:21 -0400

    disabling cors

--- scrapers/latam_scraper/app_latam.py
+++ scrapers/latam_scraper/app_latam.py
@@ -3,36 +3,32 @@ from flask import Flask, jsonify, request
 from scraper_latam import LatamDate, LatamScraper
 from utils_latam import TripOptions
 
-
 LATAM_SCRAPER = LatamScraper()
 
 app = Flask(__name__)
 
-app.config['JSON_AS_ASCII'] = False
+app.config["JSON_AS_ASCII"] = False
 
-@app.route('/flights', methods=['GET'])
+
+@app.route("/flights", methods=["GET"])
 def get_flights():
 
     data = request.json
-
     origin = data["origin"]  # Código IATA ej: SCL
     destination = data["destination"]
-    date = data["date"] # DD/MM/YYYY
+    date = data["date"]  # DD/MM/YYYY
 
     day, month, year = date.split("/")
-    
+
     latam_date = LatamDate(int(day), int(month), int(year))
     options = TripOptions(origin, destination, latam_date)
 
     flights = LATAM_SCRAPER.scrape(options)
-    
-    return jsonify({
-        "origin": origin, 
-        "destination": destination, 
-        "date": date, 
-        "flights": flights
-    })
+
+    return jsonify(
+        {"origin": origin, "destination": destination, "date": date, "flights": flights}
+    )
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=3000)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=3000)
--- src/app.py
+++ src/app.py
@@ -1,40 +1,43 @@
 from flask import Flask, request
-
+from flask_cors import CORS
 import requests
 
-
 app = Flask(__name__)
+CORS(app)
 
 
-### Flights ###
-
-@app.route("/flights", methods=['POST'])
+@app.route("/flights", methods=["POST"])
 async def list_flights():
     data_json = request.json
     flights_response = get_flights_response(data_json)
     return flights_response.content
 
+
 def get_flights_response(data_json):
     return requests.get(
-        "http://latam_scraper:3000/flights", 
-        headers = {'content-type': 'application/json'},
-        json=data_json)
+        "http://latam_scraper:3000/flights",
+        headers={"content-type": "application/json"},
+        json=data_json,
+    )
 
 
 ### Bus Trips ###
 
-@app.route("/bus_trips", methods=['POST'])
+
+@app.route("/bus_trips", methods=["POST"])
 async def list_bus_trips():
     data_json = request.json
     bus_trips_response = get_bus_trips_response(data_json)
     return bus_trips_response.content
 
+
 def get_bus_trips_response(data_json):
     return requests.get(
-        "http://turbus_scraper:6000/bus_trips", 
-        headers = {'content-type': 'application/json'},
-        json=data_json)
+        "http://turbus_scraper:6000/bus_trips",
+        headers={"content-type": "application/json"},
+        json=data_json,
+    )
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
